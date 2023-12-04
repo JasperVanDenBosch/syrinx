58645de05b31807e574694e617c39145fd8bf1cb
Jasper van den Bosch <japsai@gmail.com>
2023-12-04 17:51:54 +0000

    note in preprocess docstring

--- preprocess.py
+++ preprocess.py
@@ -1,11 +1,10 @@
 """Convert data files to content
 
-
 The archetype used will be based on the filename of the data file.
 Each row is considered one record.
 The header (1st) row will be used as keys.
+The first column is used as name for the content item.
 Each column will be converted to a variable in the front matter 
-
 """
 from __future__ import annotations
 from typing import TYPE_CHECKING, Dict

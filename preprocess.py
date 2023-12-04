2f4cc15c7d0e988b79e34281ff1cc161632a08d7
Jasper van den Bosch <japsai@gmail.com>
2023-12-04 17:50:51 +0000

    preprocess script basic functionality finished

--- preprocess.py
+++ preprocess.py
@@ -7,18 +7,24 @@ The header (1st) row will be used as keys.
 Each column will be converted to a variable in the front matter 
 
 """
+from __future__ import annotations
+from typing import TYPE_CHECKING, Dict
 from os.path import join, isdir, abspath, basename
+from os import makedirs
 from glob import glob
 import sys
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 from pandas import read_csv
+if TYPE_CHECKING:
+    from jinja2 import Template
 
 root_dir = abspath(sys.argv[1])
 assert isdir(root_dir)
 
 archetype_files = glob(join(root_dir, 'archetypes', '*.md'))
-archetypes = dict()
+archetypes: Dict[str, Template] = dict()
 env = Environment(
-    loader=FileSystemLoader('templates'),
+    loader=FileSystemLoader(join(root_dir, 'archetypes')),
     autoescape=select_autoescape()
 )
 
@@ -28,11 +34,18 @@ for fpath in archetype_files:
     archetypes[archetype_name] = env.get_template(fname)
 
 data_files = glob(join(root_dir, 'data', '*.tsv'))
+
 for fpath in data_files:
     archetype_name = basename(fpath).split('.')[0]
     if archetype_name not in archetypes:
         raise ValueError(f'No archetype for {archetype_name}')
     archetype = archetypes[archetype_name]
-    df = read_csv(fpath, sep='\t')
+
+    collection_dir = join(root_dir, 'content', archetype_name)
+    makedirs(collection_dir, exist_ok=True)
+
+    df = read_csv(fpath, sep='\t', index_col=0)
     for label, row in df.iterrows():
-        archetype.render()
+        output = archetype.render(dict(row))
+        with open(join(collection_dir, f'{label}.md'), 'w') as fhandle:
+            fhandle.write(output)

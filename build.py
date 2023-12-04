afcdf6b08ebaade546dd4cac9239cbbef6f9853b
Jasper van den Bosch <japsai@gmail.com>
2023-12-04 19:02:38 +0000

    basic build mechanism

--- build.py
+++ build.py
@@ -1,37 +1,50 @@
+from __future__ import annotations
+from os.path import abspath, relpath, isdir, basename, join
 import shutil, os, glob
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from markdown import markdown
+import sys
 
-if os.path.isdir('dist'):
-    shutil.rmtree('dist')
-os.mkdir('dist')
+root_dir = abspath(sys.argv[1])
+assert isdir(root_dir)
+content_dir = join(root_dir, 'content')
+template_dir = join(root_dir, 'theme')
+dist_dir = join(root_dir, 'dist')
 
-content = dict()
-md_fpaths = glob.glob('content/*.md')
-for fpath in md_fpaths:
-    key = os.path.basename(fpath)[:-3]
-    with open(fpath) as fhandle:
-        md_string = fhandle.read()
-    content[key] = markdown(md_string)
+if isdir(dist_dir):
+    shutil.rmtree(dist_dir)
+os.makedirs(dist_dir, exist_ok=True)
+
+env = Environment(
+    loader=FileSystemLoader(template_dir),
+    autoescape=select_autoescape()
+)
+page_template = env.get_template('page.jinja2')
+
 
 ## maybe each article should be an object with html attached, and front matter
 ## this way we can also access children
 
-env = Environment(
-    loader=FileSystemLoader('templates'),
-    autoescape=select_autoescape()
-)
+for (dirpath, dirnames, fnames) in os.walk(content_dir):
+    out_dir = dirpath.replace(content_dir, dist_dir)
+    os.makedirs(out_dir, exist_ok=True)
+    
+    for fname in fnames:
+        in_fpath = join(dirpath, fname)
+        out_fpath = join(out_dir, fname.replace('.md', '.html'))
 
-template = env.get_template('index.html')
+        with open(in_fpath) as fhandle:
+            md_string = fhandle.read()
+        content_html = markdown(md_string)
+        html = page_template.render(content=content_html)
+        with open(out_fpath, 'w') as fhandle:
+            fhandle.write(html)
 
-html = template.render(content=content)
 
-with open('dist/index.html', 'w') as fhandle:
-    fhandle.write(html)
 
-## copy images to dist 
-for fpath in glob.glob('static/*.jpg'):
-    shutil.copy(fpath, 'dist/')
+# ## copy images to dist 
+# for fpath in glob.glob('static/*.jpg'):
+#     shutil.copy(fpath, 'dist/')
 
-## copy css file
-shutil.copy('styles/index.css', 'dist/styles.css')
+# ## copy css file
+# shutil.copy('styles/index.css', 'dist/styles.css')

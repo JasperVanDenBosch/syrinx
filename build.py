93013d99925273fc8be21694a2116e5bbfa8274e
Jasper van den Bosch <japsai@gmail.com>
2024-01-06 19:37:29 +0000

    separate parsing and building, hierarchy of nodes

--- build.py
+++ build.py
@@ -1,9 +1,17 @@
 from __future__ import annotations
-from os.path import abspath, relpath, isdir, basename, join
+from typing import List, Dict
+from os.path import abspath, dirname, isdir, basename, join
 import shutil, os, glob
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from markdown import markdown
 import sys
+
+class ContentNode:
+    name: str
+    children: List[ContentNode]
+    content_html: str
+    front: Dict[str, str]
+    out_fpath: str
 
 root_dir = abspath(sys.argv[1])
 assert isdir(root_dir)
@@ -22,25 +30,80 @@ env = Environment(
 page_template = env.get_template('page.jinja2')
 
 
+
 ## maybe each article should be an object with html attached, and front matter
 ## this way we can also access children
-
+tree: Dict[str, ContentNode] = dict()
+root = ContentNode()
+root.name = ''
 for (dirpath, dirnames, fnames) in os.walk(content_dir):
     out_dir = dirpath.replace(content_dir, dist_dir)
     os.makedirs(out_dir, exist_ok=True)
+    #raise ValueError
+
+    index_index = fnames.index('index.md')
+    fnames.insert(0, fnames.pop(index_index))
     
+    indexNode = ContentNode()
+    indexNode.name = basename(dirpath)
+    if dirpath == content_dir:
+        indexNode = root
+    else:
+        parent = tree[dirname(dirpath)]
+        parent.children.append(indexNode)
+    tree[dirpath] = indexNode
     for fname in fnames:
+        name = fname.split('.')[0]
+
         in_fpath = join(dirpath, fname)
         out_fpath = join(out_dir, fname.replace('.md', '.html'))
 
         with open(in_fpath) as fhandle:
-            md_string = fhandle.read()
-        content_html = markdown(md_string)
-        html = page_template.render(content=content_html)
-        with open(out_fpath, 'w') as fhandle:
-            fhandle.write(html)
+            lines = fhandle.readlines()
+
+        markers = [l for (l, line) in enumerate(lines) if line.strip() == '---']
+        assert len(markers) == 2
+        fm_lines = [line.strip() for line in lines[1:markers[1]]]
+        fm_dict = dict()
+        for line in fm_lines:
+            parts = line.split(':')
+            key = parts[0]
+            val = line[(len(key)+1):].strip()
+            fm_dict[key] = val
+
+        md_content = ''.join(lines[markers[1]:])
+
+        if name == 'index':
+            node = indexNode
+        else:
+            node = ContentNode()
+            node.name = name
+            indexNode.children.append(node)
+        
+        node.out_fpath = out_fpath
+        node.children = []
+        node.front = fm_dict
+        node.content_html = markdown(md_content)
+
+        
 
 
+def build_node(node: ContentNode):
+    html = page_template.render(node=node)
+    with open(node.out_fpath, 'w') as fhandle:
+        fhandle.write(html)
+    for child in node.children:
+        build_node(child)
+
+
+build_node(root)
+
+""" 
+every page access to ; top level links (directory names)
+every page access to: its child nodes
+
+so its a tree object, or recursive nodes
+"""
 
 # ## copy images to dist 
 # for fpath in glob.glob('static/*.jpg'):

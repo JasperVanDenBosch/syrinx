0f0d724ca1a52ab2b133a83b46c404fed885a87b
Jasper van den Bosch <japsai@gmail.com>
2024-09-18 14:22:24 +0100

    started on tests

---
+++ tests/read.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from unittest.mock import Mock
+
+
+class ReadTests(TestCase):
+
+    def test_read_BuildPage(self):
+        """
+        FrontMatter "BuildPage" attribute:
+        - if index.md present and has "BuildPage", use this
+        - if index.md present then set "BuildPage" to true
+        - if index.md absent then set "BuildPage" to false
+        """
+        self.fail()
+
+    def test_read_Fail_if_index_missing(self):
+        """
+        FrontMatter "BuildPage" attribute:
+        - if index.md present and has "BuildPage", use this
+        - if index.md present then set "BuildPage" to true
+        - if index.md absent then set "BuildPage" to false
+        """
+        self.fail()

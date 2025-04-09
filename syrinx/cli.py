8aebb1b2cf7c551cdde5d21518e221032db92bd2
Dan Brady <daniel.brady@sheffield.ac.uk>
2025-04-09 15:28:03 +0100

    switch to argparse

--- syrinx/cli.py
+++ syrinx/cli.py
@@ -2,11 +2,20 @@ from syrinx.build import build
 from syrinx.read import read
 from syrinx.preprocess import preprocess
 from os.path import abspath, isdir
-import sys
+import argparse
 
+
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("root_dir", type=str, help="Location of root directory to build from")
+
+    return parser.parse_args()
 
 def main():
-    root_dir = abspath(sys.argv[1])
+    args = get_args()
+
+    root_dir = abspath(args.root_dir)
     assert isdir(root_dir)
     preprocess(root_dir)
     root = read(root_dir)
